03b43d42d73363413f1b4eeb5af572dac94f4aca
NyeongB <kdtoaj14@ajou.ac.kr>
2021-01-07 18:12:03 +0900

    백준 문자열 팰린드롭 만들기

--- Hash/Hash_01.py
+++ Hash/Hash_01.py
@@ -11,7 +11,7 @@ def solution(participant, completion):
     dict = {}
     for i in participant:
         if dict.get(i) == None :
-            dict[i] = 1;
+            dict[i] = 1
         else :
             dict[i] = dict.get(i) + 1
             
