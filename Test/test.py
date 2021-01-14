6e436bb8e01600b417ec332017bce7544b5babc4
NyeongB <kdtoaj14@ajou.ac.kr>
2021-01-14 21:15:00 +0900

    백준 골드5 파이프 옮기기1 (BFS, Time Over)

--- Test/test.py
+++ Test/test.py
@@ -1,13 +1,66 @@
-N, M = map(int, input().split())
+n = int(input())
 
-visited = [False] * (N+1)
-answer = []
-def dfs(depth):
-    if depth == M:
-        print(''.join(answer))
-        return
-    for i in range(1, N+1):
-        answer.append(str(i))
-        dfs(depth+1)
-        answer.pop()
-dfs(0)
+map = [list(map(int,input().split())) for _ in range(n)]
+
+queue = []
+
+def bfs():
+    queue = [(0,0,0)]   # r,c,방향 
+    count = 0
+    while queue:
+        temp = queue.pop(0)
+        x = temp[0]
+        y = temp[1]
+        d = temp[2]
+
+        if d == 0:
+            dx = x + 0
+            dy = y + 1
+            
+            if map[dx][dy] == 0:
+                if dx == n-1 and dy== n-1:
+                    count +=1
+                else :
+                    if dy+1<n:
+                        if map[dx][dy+1] == 0:
+                            queue.append([dx,dy,0])
+                    if dy+1<n and dx+1<n:
+                        if map[dx+1][dy] ==0 and map[dx][dy+1] == 0 and map[dx+1][dy+1] ==0:
+                            queue.append([dx,dy,2])
+                
+        elif d == 1:
+            dx = x + 1
+            dy = y + 0
+            
+            if map[dx][dy] == 0:
+                if dx == n-1 and dy== n-1:
+                    count +=1
+                else :
+                    if dx+1<n:
+                        if map[dx+1][dy] == 0:
+                            queue.append([dx,dy,1])
+                    if dy+1<n and dx+1<n:
+                        if map[dx+1][dy] ==0 and map[dx][dy+1] == 0 and map[dx+1][dy+1] ==0:
+                            queue.append([dx,dy,2])
+        elif d == 2:
+            dx = x + 1
+            dy = y + 1
+            
+            if map[dx][dy] == 0:
+                if dx == n-1 and dy== n-1:
+                    count +=1
+                else :
+                    if dy+1<n:
+                        if map[dx][dy+1] == 0:
+                            queue.append([dx,dy,0])
+                    if dx+1<n:
+                        if map[dx+1][dy] == 0:
+                            queue.append([dx,dy,1])
+                    if dy+1<n and dx+1<n:
+                        if map[dx+1][dy] ==0 and map[dx][dy+1] == 0 and map[dx+1][dy+1] ==0:
+                            queue.append([dx,dy,2])
+
+    return count
+
+print(bfs())
+    
