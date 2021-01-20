f1c59065e7ec824abf315f0f221e6a07c44bc55c
NyeongB <kdtoaj14@ajou.ac.kr>
2021-01-20 10:16:30 +0900

    파이썬 팁 노트

--- Test/test.py
+++ Test/test.py
@@ -1,66 +1,9 @@
-n = int(input())
-
-map = [list(map(int,input().split())) for _ in range(n)]
-
-queue = []
-
-def bfs():
-    queue = [(0,0,0)]   # r,c,방향 
-    count = 0
-    while queue:
-        temp = queue.pop(0)
-        x = temp[0]
-        y = temp[1]
-        d = temp[2]
-
-        if d == 0:
-            dx = x + 0
-            dy = y + 1
-            
-            if map[dx][dy] == 0:
-                if dx == n-1 and dy== n-1:
-                    count +=1
-                else :
-                    if dy+1<n:
-                        if map[dx][dy+1] == 0:
-                            queue.append([dx,dy,0])
-                    if dy+1<n and dx+1<n:
-                        if map[dx+1][dy] ==0 and map[dx][dy+1] == 0 and map[dx+1][dy+1] ==0:
-                            queue.append([dx,dy,2])
-                
-        elif d == 1:
-            dx = x + 1
-            dy = y + 0
-            
-            if map[dx][dy] == 0:
-                if dx == n-1 and dy== n-1:
-                    count +=1
-                else :
-                    if dx+1<n:
-                        if map[dx+1][dy] == 0:
-                            queue.append([dx,dy,1])
-                    if dy+1<n and dx+1<n:
-                        if map[dx+1][dy] ==0 and map[dx][dy+1] == 0 and map[dx+1][dy+1] ==0:
-                            queue.append([dx,dy,2])
-        elif d == 2:
-            dx = x + 1
-            dy = y + 1
-            
-            if map[dx][dy] == 0:
-                if dx == n-1 and dy== n-1:
-                    count +=1
-                else :
-                    if dy+1<n:
-                        if map[dx][dy+1] == 0:
-                            queue.append([dx,dy,0])
-                    if dx+1<n:
-                        if map[dx+1][dy] == 0:
-                            queue.append([dx,dy,1])
-                    if dy+1<n and dx+1<n:
-                        if map[dx+1][dy] ==0 and map[dx][dy+1] == 0 and map[dx+1][dy+1] ==0:
-                            queue.append([dx,dy,2])
-
-    return count
-
-print(bfs())
-    
+a = set()
+a.add(1)
+a.add(2)
+a.add(3)
+print(4 in a)
+print(3 in a)
+a.remove(1)
+a.pop()
+print(len(a))
--- Test/test2.py
+++ Test/test2.py
@@ -1,8 +1,4 @@
-import heapq
+a = [3,2,3,4]
 
-a = [5,4,3,2,1,]
-
-heapq.heapify(a)
-heapq.heappush(a,0)
-print(heapq.heappop(a))
-print(a)
+for i, j in enumerate(a) :
+    print(i,j)
