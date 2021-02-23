46652dd217e99d64b5ac902072968dd099701660
NyeongB <kdtoaj14@ajou.ac.kr>
2021-02-23 22:59:24 +0900

    Tip update

--- Test/test.py
+++ Test/test.py
@@ -1,7 +1,16 @@
-import sys
+n = 5
+c = [list(map(int,input().split())) for _ in range(n)]
 
-a = []
+print(c)
 
-a = list(map(int, sys.stdin.readline().rstrip().split()))
+d = [list(map(int, input())) for _ in range(n)]
 
-print(a)
+print(d[1][2])
+
+'''
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+'''
