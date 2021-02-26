43991a066a7726b0ce9ec6c058bf428fe7d84dbe
NyeongB <kdtoaj14@ajou.ac.kr>
2021-02-26 22:30:49 +0900

    update tip

--- Test/test.py
+++ Test/test.py
@@ -1,16 +1,6 @@
-n = 5
-c = [list(map(int,input().split())) for _ in range(n)]
+from itertools import combinations
 
-print(c)
+nums = [1,2,3,4,5]
+com = list(combinations(nums, 4))
 
-d = [list(map(int, input())) for _ in range(n)]
-
-print(d[1][2])
-
-'''
-1 1 1 1 1
-1 1 1 1 1
-1 1 1 1 1
-1 1 1 1 1
-1 1 1 1 1
-'''
+print(com)
