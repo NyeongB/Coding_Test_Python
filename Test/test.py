a8a338a7244570d1b958569292db4f597054a2fa
NyeongB <kdtoaj14@ajou.ac.kr>
2021-03-22 20:06:54 +0900

    백준 실버1 골드바흐의 추측

--- Test/test.py
+++ Test/test.py
@@ -1,4 +1,40 @@
-s = input()
+import math
 
-for _ in range(3):
-    print(s, end=' ')
+s = "Goldbach's conjecture is wrong."
+
+
+def prime(n):
+    t = int(math.sqrt(n))
+    for i in range(2, t+1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+
+a = []
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    a.append(n)
+
+for i in a:
+    t1 = i
+    flag = 1
+    for j in range(3, i, 2):
+        if prime(j):
+            if prime(t1 - j):
+                print(str(t1)+ " = " + str(j) + " + "+ str(t1-j))
+                flag = 0
+                break
+    if flag == 1:
+        print(s)
+
+'''
+pypy 532ms
+python3 3122ms
+3초 넘어갔는데 맞았다고 하는데 이거 체크
+'''
+
+
