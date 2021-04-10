6eb074dc2c165c59f36dc4ad86d8ac5daad4be98
NyeongB <kdtoaj14@ajou.ac.kr>
2021-04-10 16:03:13 +0900

    Hash + Set practice

--- Test/test.py
+++ Test/test.py
@@ -1,7 +1,25 @@
-s = 'gfedcbZ'
+orders = ['choi pizza hamberger steak', 'lee noodle hamberger pasta', 'park kimchi sandwich pizza',
+'choi hamberger pasta steak']
 
-arr = list(s)
+def solution(orders):
+    answer = []
+    hm = {}
+    for i in orders:
+        a = i.split()
+        hm[a[0]] = set()
+        for j in range(1,len(a)):
+            hm[a[0]].add(a[j])
 
-sorted(arr)
+    m = []
 
-print(arr)
+    for k,v in hm.items():
+        m.append(len(v))
+
+    for k,v in hm.items():
+        if max(m) == len(v):
+            answer.append(k)    
+    a.sort()
+    return answer
+
+
+print(solution(orders))
