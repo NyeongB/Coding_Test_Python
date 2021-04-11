6e2616e0073cbb24f9060f7d51b55fd78a49ba28
NyeongB <kdtoaj14@ajou.ac.kr>
2021-04-11 18:11:57 +0900

    백준 실버 2 부분수열의 합

--- Test/test.py
+++ Test/test.py
@@ -1,7 +1,27 @@
-h = {}
-h[1] = 1
-h[2] = 2
-h[3] = 3
 
-h.pop(1)
-print(h)
+n = int(input())
+
+data = []
+answer = []
+
+for _ in range(n):
+    data.append(int(input()))
+
+def recursion(n,sum):
+    if n < sum:
+        return 0
+    elif n == sum:
+        return 1
+
+    result = 0
+
+    for i in range(1, 4):
+        result += recursion(n,sum+i)
+    
+    return result
+
+for i in data:
+    answer.append(recursion(i,0))
+
+for i in answer:
+    print(i)
