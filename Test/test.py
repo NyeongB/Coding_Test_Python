310c99fcb8a9da80d400eb35b23805809126b9bb
NyeongB <kdtoaj14@ajou.ac.kr>
2021-04-11 13:14:21 +0900

    Tip

--- Test/test.py
+++ Test/test.py
@@ -1,25 +1,11 @@
-orders = ['choi pizza hamberger steak', 'lee noodle hamberger pasta', 'park kimchi sandwich pizza',
-'choi hamberger pasta steak']
-
-def solution(orders):
-    answer = []
-    hm = {}
-    for i in orders:
-        a = i.split()
-        hm[a[0]] = set()
-        for j in range(1,len(a)):
-            hm[a[0]].add(a[j])
-
-    m = []
-
-    for k,v in hm.items():
-        m.append(len(v))
-
-    for k,v in hm.items():
-        if max(m) == len(v):
-            answer.append(k)    
-    a.sort()
-    return answer
+from collections import deque
 
 
-print(solution(orders))
+arr = [1,1,1,1,1,9,1]
+
+q = deque((v,i) for i, v in enumerate(arr))
+#q = deque((i,v) for i, v in enumerate(arr))
+
+print(max(q))
+
+#좀 충격인게 v,i 로 뽑아야지 맥스 가능하고 i, v로 고대로 뽑으면 밸류 부분에서 max()가 안됌
