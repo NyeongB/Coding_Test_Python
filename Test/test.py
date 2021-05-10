f5dc3e7a1836d3b1042fda30040424d399bdbcc5
NyeongB <kdtoaj14@ajou.ac.kr>
2021-05-10 21:44:29 +0900

    백준 실버 3 피보나치는 지겨웡~

--- Test/test.py
+++ Test/test.py
@@ -1,3 +1,47 @@
-s = input('숫자를 입력하세요: ')
+dx = [1,0,-1,0] # 남 동 북 서 
+dy = [0,1,0,-1]
 
-print(s)
+def rotate(x1,y1,x2,y2):
+    x1 -= 1
+    y1 -= 1
+    x2 -= 1
+    y2 -= 1
+    idx = 0
+    x = x1
+    y = y1
+    Min = 1e9
+    temp = arr[x][y]
+    while idx < 4:
+        nx = x + dx[idx]
+        ny = y + dy[idx]
+        
+        if x1 <= nx <= x1 and y1 <= ny <= y2:
+            arr[x][y] = arr[nx][ny]
+            x = nx
+            y = ny
+        else:
+            idx +=1
+            
+    for i in arr:
+        print(' '.join(map(str,i)),end='\n')
+        
+    return Min
+    
+
+
+def solution(rows, columns, queries):
+    answer = []
+    
+    arr = list( ([0]*columns) for _ in range(rows))
+    num = 1
+    for i in range(rows):
+        for j in range(columns):
+            arr[i][j] = num
+            num += 1
+    
+    for x1,y1,x2,y2 in queries:
+        rotate(x1,y1,x2,y2)
+    
+    
+    
+    return answer
